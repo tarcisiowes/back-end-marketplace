d14e9864eefc09a75b1e46970cf630d8700977e9
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-06-30 15:25:36 -0300

    heroku-db-config

--- src/conexao.js
+++ src/conexao.js
@@ -19,11 +19,16 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: '123456',          
-        database: 'market_cubos'
+        host: 'ec2-52-45-179-101.compute-1.amazonaws.com',
+        user: 'wnabpdrpcyoklj',
+        password: '0aa9a16562d0e8ddc1d81edc6fe1c81fbe358c3ae7d139df29e10b38a47a8df8',          
+        database: 'd3s3mi5q8k71c4',
+        Port: 5432,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 })
 
 module.exports = knex
+
